4d53f46865811b4ed663b22ec0b3276c7d0a7308
heinzhex <B1Z9IC@protonmail.com>
2025-03-21 17:20:51 +0900

    chatroom update

--- scibox/05_chatroom_widget.js
+++ scibox/05_chatroom_widget.js
@@ -1,7 +1,7 @@
 const chatroomWidget = {
     html: `
         <div class="chatroom-widget">
-            <button class="chat-toggle" onclick="window.openUploader()" title="Open Chatroom">
+            <button class="chat-toggle" onclick="window.openChatroom()" title="Open Chatroom">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                 </svg>
@@ -76,7 +76,7 @@ const chatroomWidget = {
     `,
     js: `
         // Make function globally accessible
-        window.openUploader = function() {
+        window.openChatroom = function() {
             window.open('https://uploader.irys.xyz/CuTqKCLYvoFnYvSrFkorpKT9WVuMhFNXbVmFZNjzFDMb', '_blank');
         };
 
